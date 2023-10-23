49822de09ac2d92243725ed842f807ea29c97daf
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2023-10-23 16:53:36 +0200

    update

--- segmentation.py
+++ segmentation.py
@@ -3,7 +3,7 @@ import myfunctions as mf            # type: ignore
 
 if __name__ == '__main__':
     for exp in mf.exp_list():
-        sequence = mf.read_4Dsequence(exp, OS='Linux')
+        sequence = mf.read_4Dsequence(exp, OS='Tyrex')
         threshold = mf.find_threshold(sequence)
         segmented_sequence = mf.segment4D(sequence, threshold)
         mf.save_segmented_sequence(segmented_sequence, exp, OS='Linux')
