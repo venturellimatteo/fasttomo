295949231456faf6829d877a651b96039a7d1349
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2023-10-27 09:34:39 +0200

    code update

--- memmap/segmentation2.py
+++ memmap/segmentation2.py
@@ -1,3 +1,3 @@
 import myfunctions as mf
 
-mf.segment4D(exp=mf.exp_list()[1], end_time=109, OS='Linux', smallest_3Dvolume=50, smallest_4Dvolume=250)
+mf.segment4D(exp=mf.exp_list()[1], OS='Linux', smallest_3Dvolume=50, smallest_4Dvolume=250)
