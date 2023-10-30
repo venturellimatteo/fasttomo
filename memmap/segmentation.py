0cb9090f7463a3c332f48416c29bde5afbfa0b0a
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2023-10-30 16:46:29 +0100

    code update

--- memmap/segmentation.py
+++ memmap/segmentation.py
@@ -1,5 +1,5 @@
 import myfunctions as mf
 
-hypervolume_mask = mf.segment4D(exp=mf.exp_list()[1], OS='Linux')
+hypervolume_mask = mf.segment4D(exp=mf.exp_list()[2], OS='Linux')
 
 mf.filtering4D(hypervolume_mask)
