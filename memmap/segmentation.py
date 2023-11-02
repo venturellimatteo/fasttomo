ef3b88fd9ce7ac21c68c8d17f57e3dd7d74c6cbd
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2023-11-02 09:08:01 +0100

    code update

--- memmap/segmentation.py
+++ memmap/segmentation.py
@@ -1,5 +1,29 @@
 import myfunctions as mf
+from numpy.lib.format import open_memmap
+from numpy import save
+import os
 
-hypervolume_mask = mf.segment4D(exp=mf.exp_list()[2], OS='Linux')
+segment = False
+filtering = False
+motion = True
+exp = mf.exp_list()[0]
+OS = 'Linux'
 
-mf.filtering4D(hypervolume_mask)
+if segment:
+    hypervolume_mask = mf.segment4D(exp=exp, OS=OS)
+else:
+    hypervolume_mask = open_memmap(os.path.join(mf.OS_path(exp, OS), 'hypervolume_mask.npy'), mode='r')
+if filtering:
+    mf.filtering4D(hypervolume_mask)
+if motion:
+    position, volume, speed, volume_exp_rate, avg_volume, agg_number = mf.motion_matrix(hypervolume_mask, exp=exp)
+    if not os.path.exists(os.path.join(mf.OS_path(exp, OS), 'motion_properties')):
+        os.makedirs(os.path.join(mf.OS_path(exp, OS), 'motion_properties'))
+    print('Saving properties...')
+    save(os.path.join(mf.OS_path(exp, OS), 'motion_properties', 'position.npy'), position)
+    save(os.path.join(mf.OS_path(exp, OS), 'motion_properties', 'volume.npy'), volume)
+    save(os.path.join(mf.OS_path(exp, OS), 'motion_properties', 'speed.npy'), speed)
+    save(os.path.join(mf.OS_path(exp, OS), 'motion_properties', 'volume_exp_rate.npy'), volume_exp_rate)
+    save(os.path.join(mf.OS_path(exp, OS), 'motion_properties', 'avg_volume.npy'), avg_volume)
+    save(os.path.join(mf.OS_path(exp, OS), 'motion_properties', 'agg_number.npy'), agg_number)
+    print('Done!')
