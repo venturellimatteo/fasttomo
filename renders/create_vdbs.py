c7b7ce6c0b61a3e3e1a33e6373a4d72d6a1728dc
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2023-11-16 16:09:38 +0100

    reorganization

---
+++ renders/create_vdbs.py
@@ -0,0 +1,56 @@
+import pyopenvdb as vdb                     # type: ignore
+import numpy as np                          # type: ignore
+from numpy.lib.format import open_memmap    # type: ignore
+import os
+
+# function that saves the vdb file related to a given label given the map and the path
+def save_vdb(temp, obj_path):
+    grid = vdb.Int32Grid()
+    grid.copyFromArray(temp.astype(int))
+    grid.transform  = vdb.createLinearTransform(voxelSize=0.01)
+    grid.gridClass = vdb.GridClass.FOG_VOLUME
+    grid.name = 'density'
+    vdb.write(obj_path, grid)
+    return None
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return None
+
+if __name__ == "__main__":
+    parent_dir = '/Volumes/T7/Thesis'
+    exp_list = ['P28A_FT_H_Exp1','P28A_FT_H_Exp2','P28A_FT_H_Exp3_3', 'P28A_FT_H_Exp4_2', 'P28B_ISC_FT_H_Exp2','VCT5_FT_N_Exp1',
+                'VCT5_FT_N_Exp3','VCT5_FT_N_Exp4','VCT5_FT_N_Exp5','VCT5A_FT_H_Exp2','VCT5A_FT_H_Exp5']
+    
+    for exp in exp_list:
+        # defining and creating the folders where the vdb objects will be saved
+        exp_dir = os.path.join(parent_dir, exp)
+        vdb_dir = os.path.join(exp_dir, 'vdbs')
+        create_folder(vdb_dir)
+        # opening the 4D segmentation map as a memmap to save space on the RAM
+        mask = open_memmap(os.path.join(exp_dir, 'hypervolume_mask.npy'), mode='r')
+        # defining the number of time steps contained in the 4D volume
+        time_steps = mask.shape[0]
+        # determining the maximum value for the labels in the map
+        max_label = np.max(mask)
+        np.save(os.path.join(exp_dir, 'max_label.npy'), max_label)
+
+        for t in range(time_steps):
+            # defining the vdbs folder path associated to this time step
+            vdb_time_dir = vdb_dir + '/time-' + str(t)
+            # creating the folder if it doesn't exist already
+            create_folder(vdb_time_dir)
+            # creating a numpy array containing the labels present in the current volume
+            labels = np.unique(mask[t])
+            
+            for label in labels:
+                # we do not create any objects associated with the background
+                if label==0:
+                    continue
+                # creating a map containing all zeros except where the specific label is present (value=1000)
+                temp = np.where(mask[t]==label, 1000, 0)
+                # defining the name and the path of the vdb objects that are being saved
+                obj_path = os.path.join(vdb_time_dir, str(label) + '.vdb')
+                # constructing the vdb grid for the label and saving the vdb object in the folder associated to that time instant
+                save_vdb(temp, obj_path)
---
+++ renders/render.py
@@ -0,0 +1,114 @@
+import bpy                                  # type: ignore
+import numpy as np                          # type: ignore
+import os
+
+# function that returns a color tuple given the label and the maximum value of the labels
+def color(x, max_label):
+    step = max_label/6
+    i = x//int(step)
+    R = [0.25*(x%step)/step, 0.25*(x%step)/step, 0.25, 0, 0.25, 0]
+    G = [0.25, 0, 0.25*(x%step)/step, 0.25*(x%step)/step, 0, 0.25]
+    B = [0, 0.25, 0, 0.25, 0.25*(x%step)/step, 0.25*(x%step)/step]
+    return (R[i], G[i], B[i], 0.5)
+    
+# function that modifies the properties of the object associated to a specific label
+def modify_properties(so, label, index, max_label):
+    mat = bpy.data.materials.new(name='Material_'+str(label))
+    so.data.materials.append(mat)
+    mat.use_nodes = True
+    if label==1:
+        # if the object is the external shell the color is set to black
+        mat.node_tree.nodes["Principled Volume"].inputs[0].default_value = (0,0,0,1)
+        # if the object is the external shell the density is set to a low value
+        mat.node_tree.nodes["Principled Volume"].inputs[2].default_value = 0.003
+    else:    
+        # modifying the color
+        mat.node_tree.nodes["Principled Volume"].inputs[0].default_value = color(index, max_label)
+        # modifiying the density
+        mat.node_tree.nodes["Principled Volume"].inputs[2].default_value = 0.5
+    # modifying the anisotropy
+    mat.node_tree.nodes["Principled Volume"].inputs[4].default_value = -0.5
+    return None
+
+# function that modifies the properties of the rendering engine
+def modify_engine():
+    bpy.context.scene.render.engine = 'CYCLES'
+    bpy.context.scene.cycles.time_limit = 30
+    bpy.context.scene.cycles.use_adaptive_sampling = True
+    bpy.context.scene.cycles.device = 'GPU'
+    bpy.context.scene.render.resolution_x = 2160
+    bpy.context.scene.render.resolution_y = 2160
+    bpy.context.scene.render.image_settings.file_format = 'JPEG'
+    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (1, 1, 1, 1)
+    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[1].default_value = 1.5
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return None
+
+
+
+if __name__ == "__main__":
+    parent_dir = '/Volumes/T7/Thesis'
+    exp_list = ['P28A_FT_H_Exp1','P28A_FT_H_Exp2','P28A_FT_H_Exp3_3','P28B_ISC_FT_H_Exp2','VCT5_FT_N_Exp1',
+                'VCT5_FT_N_Exp3','VCT5_FT_N_Exp4','VCT5_FT_N_Exp5','VCT5A_FT_H_Exp2','VCT5A_FT_H_Exp5']
+    # modify rendering engine
+    modify_engine()
+    
+    for exp in exp_list:
+        # defining and creating the folders where the vdb objects will be saved
+        exp_dir = os.path.join(parent_dir, exp)
+        vdb_dir = os.path.join(exp_dir, 'vdbs')
+        time_list = os.listdir(vdb_dir)
+
+        render_dir = os.path.join(exp_dir, 'renders')
+        create_folder(render_dir)
+
+        side_dir = os.path.join(render_dir, 'side view')
+        create_folder(side_dir)
+
+        top_dir = os.path.join(render_dir, 'top view')
+        create_folder(top_dir)        
+
+        max_label = np.load(os.path.join(exp_dir, 'max_label.npy'))
+        shuffled_indexes = np.random.permutation(max_label+1)
+
+        for t in time_list:
+            time_dir = os.path.join(vdb_dir, t)
+            obj_names_list = os.listdir(time_dir)
+            
+            for obj_name in obj_names_list:
+                label = int(obj_name.split('.')[0])
+                index = shuffled_indexes[label]
+                obj_path = os.path.join(time_dir, obj_name)
+                bpy.ops.object.volume_import(filepath=obj_path, files=[], location=(-2.5,-2.5,1.3), rotation=(0, np.pi/2, 0))
+                # selecting the new object
+                so = bpy.context.active_object
+                # modifying the properties of the new object
+                modify_properties(so, label, index, max_label+1)
+
+            # creating the light and modifying its intensity
+            bpy.ops.object.light_add(type='POINT', radius=1, align='WORLD', location=(10, 0, 10), scale=(1, 1, 1))
+            bpy.context.active_object.data.energy = 1000
+
+            # ---------------------- SIDE VIEW ---------------------- #
+
+            bpy.ops.object.camera_add(enter_editmode=False, location=(0, 0, 15))
+            bpy.context.active_object.data.type = 'ORTHO'
+            bpy.context.object.data.ortho_scale = 10
+            bpy.context.scene.camera = bpy.context.active_object
+                
+            bpy.context.scene.render.filepath = os.path.join(top_dir, t + '.jpg')
+            bpy.ops.render.render(write_still=True)
+            bpy.ops.object.delete()
+
+            # ---------------------- TOP VIEW ---------------------- #
+            bpy.ops.object.camera_add(enter_editmode=False, location=(15, 0, 0), rotation=(np.pi/2, 0, np.pi/2))
+            bpy.context.active_object.data.type = 'ORTHO'
+            bpy.context.scene.camera = bpy.context.active_object
+            
+            # removing objects for each label related to current time step        
+            for i in range(len(bpy.data.objects)):
+                bpy.data.objects[i].select_set(True)
+            bpy.ops.object.delete() 
