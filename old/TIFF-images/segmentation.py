af79eab17bf4e37e247a1db2d9e578579e2573c9
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2024-01-19 10:00:54 +0100

    directory update

--- old/TIFF-images/segmentation.py
+++
@@ -1,9 +0,0 @@
-import numpy as np                  # type: ignore    
-import myfunctions as mf            # type: ignore
-
-if __name__ == '__main__':
-    for exp in mf.exp_list():
-        sequence = mf.read_4Dsequence(exp, OS='Tyrex')
-        threshold = mf.find_threshold(sequence)
-        segmented_sequence = mf.segment4D(sequence, threshold, save=True, OS='Tyrex')
-    print('Done!')
