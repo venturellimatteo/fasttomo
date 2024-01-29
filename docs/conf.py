9ad2c2f881b08149f79805e09265b161a3b7e614
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2024-01-29 20:24:04 +0100

    pydata sphinx theme update

--- docs/conf.py
+++ docs/conf.py
@@ -8,25 +8,34 @@
 
 import os
 
-project = 'fasttomo'
-copyright = '2024, Matteo Venturelli'
-author = 'Matteo Venturelli'
-release = '0.1'
+project = "fasttomo"
+copyright = "2024, Matteo Venturelli"
+author = "Matteo Venturelli"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['autoapi.extension', 'sphinx.ext.napoleon']
-autoapi_dirs = ['../src/fasttomo']
-autoapi_options = ['members', 'undoc-members', 'show-inheritance', 'show-module-summary', 'special-members', 'imported-members'] # 
+extensions = ["autoapi.extension", "sphinx.ext.napoleon"]
+autoapi_dirs = ["../src/fasttomo"]
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "special-members",
+    "imported-members",
+]  #
 napoleon_numpy_docstring = True
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes", ]
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_theme_path = [
+    "_themes",
+]
+html_static_path = ["_static"]
